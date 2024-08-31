3b9022cd1f7178230a7665848e6c8df54ae21bb7
Jaeung Cheon <cheonjaewoong@gmail.com>
2024-08-31 14:46:44 +0900

    Apply maven central publishing plugin

--- simplecarousel/build.gradle.kts
+++ simplecarousel/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.maven.publish)
 }
 
 android {
@@ -25,4 +28,37 @@ android {
 dependencies {
     implementation(libs.androidx.core)
     api(libs.androidx.recyclerview)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates("${project.group}", "simplecarousel", "${project.version}")
+
+    pom {
+        name.set("SimpleCarousel for Android")
+        description.set("Simple components to make carousel UI for Android.")
+        url.set("https://github.com/cheonjaeung/simplecarousel-android")
+
+        licenses {
+            license {
+                name.set("Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("cheonjaeung")
+                name.set("Jaeung Cheon")
+                email.set("cheonjaewoong@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/cheonjaeung/simplecarousel-android")
+            connection.set("scm:git:git://github.com/cheonjaeung/simplecarousel-android.git")
+            developerConnection.set("scm:git:ssh://git@github.com/cheonjaeung/simplecarousel-android.git")
+        }
+    }
 }
